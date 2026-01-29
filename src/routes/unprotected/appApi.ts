f1936ca9d6c3c15de83ffc4ff89765f1dded19ef
stephen-anu-pc <oyekanstephen6@gmail.com>
2026-01-29 20:31:34 +0100

    small changes

--- src/routes/unprotected/appApi.ts
+++ src/routes/unprotected/appApi.ts
@@ -4,7 +4,7 @@ import { subscriptionController} from '@/controllers/unprotected';
 export const router = express.Router();
 
 // subscription routes
-router.route(`/subscription/create`).post(catchErrors(subscriptionController.create));
-router.route(`/subscription/read/:id`).get(catchErrors(subscriptionController.read));
-router.route(`/subscription/update/:id`).patch(catchErrors(subscriptionController.update));
-router.route(`/subscription/delete/:id`).delete(catchErrors(subscriptionController.delete));
+router.route(`/subscription`).post(catchErrors(subscriptionController.create));
+router.route(`/subscription/:id`).get(catchErrors(subscriptionController.read));
+router.route(`/subscription/:id`).patch(catchErrors(subscriptionController.update));
+router.route(`/subscription/:id`).delete(catchErrors(subscriptionController.delete));
